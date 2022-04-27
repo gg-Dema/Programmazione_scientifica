242f4c09b33210be6dd5728ce0881461fd3cb984
gg-Dema <gg.dema.rm.22@gmail.com>
2022-04-27 17:51:55 +0200

    pointer op & file

---
+++ 3_pointer/10_lettura_scrittura.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 100
+int main(int argc, char const *argv[])
+{
+    FILE *fp; //puntatore di tipo FILE
+    int i, k; 
+    double x, data[LEN];
+
+    //apertura file, scrittura numeri random 
+    if ( (fp = fopen("3_pointer/random.dat", "w+")) == NULL){
+        printf("ERROR In f_open\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (i=0; i<=LEN; i++){
+        x = (double) rand() / (double) RAND_MAX; //random normalizzato 0 1
+        fprintf(fp,   "%d %f\n", i, x); 
+    }   
+    //la seguente funzione non viene scritta all'interno del file
+    fprintf(fp, "stringa per causare errori");
+    fclose(fp);
+
+    //lettura file, lettura numeri all'interno
+    fp = fopen("3_pointer/random.dat", "r+") ; //r+ lettura e scrittura 
+    for (i=0; i<=LEN; i++){
+        fscanf(fp, "%d %lf", &k, &x); 
+        *(data + (LEN - k)) = x; 
+    }
+
+    fprintf(fp, "\n*** INVERSIONE ***\n"); 
+    for(i=0; i<=LEN; i++){
+        fprintf(fp, "%d %lf\n", i, *(data +i-1) );
+    }
+    fclose(fp);
+
+    return 0;
+}
---
+++ 3_pointer/11_bubblesort_pointer.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define MAX_NUM 1000
+#define MAX_FNAME 100
+
+int main(int argc, char const *argv[])
+{
+    char outputFile[MAX_FNAME + 1], c; 
+    FILE *fp;
+
+    int i=0, j, n; 
+    double data[MAX_NUM], temp, *lastpd, *pd, *qd; 
+    //inserisco un input char by char
+    printf("Inserire nome del file di output:\n"); 
+    while( (c = getchar()) != '\n' && i < MAX_FNAME){
+        outputFile[i++] = c; 
+    }
+    outputFile[i++] = '\0';
+
+    if ( (fp = fopen(outputFile, "w+")) == NULL){
+        printf("Errore nell'apertura del file %s\n", outputFile);
+        exit(EXIT_FAILURE); 
+    }
+    fprintf(fp, "**** SEQUENZA DATI ORIGINALI ****\n"); 
+    for(i=0; i<MAX_NUM; i++){
+        *(data + i) = (double)random() / (double)RAND_MAX; 
+        fprintf(fp, "%d, %f\n", i, *(data+i)); 
+    }
+    fprintf(fp, "**** TERMINE DATI ORIGINALI ****\n"); 
+
+    lastpd = data + MAX_NUM -1; //puntatore a ultimo elemento
+    for (pd= data; pd < lastpd; pd++){
+        for(qd=lastpd; qd>pd; qd--){
+            if( *(qd-1) > *qd){
+                temp = *(qd-1);
+                *(qd-1) = *(qd);
+                *(qd) = temp; 
+            }
+        }
+    }
+    fprintf(fp, "**** SEQUENZA DATI ORDINATi *****\n");
+    for(i=0; i<MAX_NUM; i++){
+        fprintf(fp, "%d, %lf\n", i, *(data+i));
+        //non puoi usare pd perche ormai punta al penultimo elemento 
+        //( ciclo for precedente)
+    }  
+    fclose(fp);
+    fflush(0); //chiude tutti i file aperti, causa 0
+    //alternative fflush(fp); chiude solo il file indicato da fp
+    //la scrittura su file avviene solo post chiusura file
+
+    return 0;
+}

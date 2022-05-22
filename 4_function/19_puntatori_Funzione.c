d8a98fd6a54c9f663f5b1378f36807f7203625e7
gg-Dema <gg.dema.rm.22@gmail.com>
2022-05-22 12:47:29 +0200

    add gitingore

--- 4_function/19_puntatori_Funzione.c
+++ 4_function/19_puntatori_Funzione.c
@@ -9,7 +9,8 @@ a funzione per passare la funzione interessata al limite incrementale
 double func(double); 
 double derivative( double (*)(double), double, double);
 //commento personale, credo che (*)(double) sia il generico nome del
-//puntatore a funzione, La funzione puntata ritorna un double
+//puntatore a funzione, La funzione puntata ritorna un double e 
+//prendere un double in input
 
 int main(int argc, char const *argv[])
 {
@@ -30,11 +31,10 @@ double derivative(double (*f)(double), double x, double epsilon){
 
 
 /*
-    Questo programma, come tutti i programmi iteratiivi di tipo double, 
+    Questo programma, come tutti i programmi iterativi di tipo double, 
     verifica gli stessi errori di approssimazione per somme o differenze di numeri troppo grandi
     o troppo piccoli. 
     Soluzione di cio sarebbe costruire la stessa implementazione per le stringhe.
     Rifarsi al listato 17, nel capitolo 4.
-    Se esso è assente, riprovare nel futuro
-    
+    Se il 17 è assente, ci sto ancora lavorando
 */
